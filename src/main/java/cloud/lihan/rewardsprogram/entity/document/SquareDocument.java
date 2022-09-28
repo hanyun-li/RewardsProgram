0aab2bca05311a6d69164c58b641bf6aefac415a
李汉云 <739125492@qq.com>
2022-09-28 17:57:09 +0800

    定义帖子文档的数据结构

---
+++ src/main/java/cloud/lihan/rewardsprogram/entity/document/SquareDocument.java
@@ -0,0 +1,37 @@
+package cloud.lihan.rewardsprogram.entity.document;
+
+import cloud.lihan.rewardsprogram.common.documents.BaseDocument;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 帖子信息文档
+ *
+ * @author hanyun.li
+ * @createTime 2022/09/28 17:32:00
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class SquareDocument extends BaseDocument {
+
+    /**
+     * 用户ID
+     */
+    private String userId;
+
+    /**
+     * 帖子内容
+     */
+    private String content;
+
+    /**
+     * 图片url集合(注：数据结构为Map转的json字符串)
+     */
+    private String imgUrls;
+
+    /**
+     * 给这个帖子点赞的用户ID集合(注：数据结构为List转的json字符串)
+     */
+    private String likeUserIds;
+
+}
