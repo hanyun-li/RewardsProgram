309db72baeebc2ff936667f4045b3c163e70ec1b
lihanyun <739125492@qq.com>
2024-11-02 23:11:16 +0800

    迭代

--- src/main/java/cloud/lihan/rewardsprogram/RewardsProgramApplication.java
+++ src/main/java/cloud/lihan/rewardsprogram/RewardsProgramApplication.java
@@ -2,7 +2,6 @@ package cloud.lihan.rewardsprogram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
